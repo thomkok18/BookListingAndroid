ce2fef778aee10e58a370a786786ab768d033441
Thom Kok <tkok@roc-dev.com>
2017-05-24 10:08:47 +0200

    update

--- app/src/main/java/com/example/android/booklisting/MainActivity.java
+++ app/src/main/java/com/example/android/booklisting/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.android.booklisting;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mEmptyStateTextView;
+
+    public static final String LOG_TAG = MainActivity.class.getName();
 
     private static final String USGS_REQUEST_URL = "https://www.googleapis.com/books/v1/volumes?q=android&maxResults=1";
 
